05763d659d1cb20fa9288817991024baf6ca7939
awai-tyzend <awais@tyzend.com>
2025-02-16 23:25:51 +0500

    herosection change

--- app/components/hero-section/HeroSection.tsx
+++ app/components/hero-section/HeroSection.tsx
@@ -9,18 +9,18 @@ const HeroSection = () => {
       <div className="hero-grid">
         <div className="hero-text">
           <h1 className="hero-title">
-            <span className="hero-greeting">Hi, I am{" "}</span>
+            <span className="hero-greeting">Hi, I am </span>
             <br />
             <TypeAnimation
               sequence={[
-                'Awais Akhtar',
+                "Awais Akhtar",
                 1000,
-                'Web Developer',
+                "Web Developer",
                 1000,
-                'Frontend Developer',
+                "Frontend Developer",
                 1000,
-                'React Developer',
-                1000
+                "React Developer",
+                1000,
               ]}
               wrapper="span"
               speed={40}
@@ -29,11 +29,27 @@ const HeroSection = () => {
             />
           </h1>
           <p className="hero-description">
-          I am a Frontend Developer with 1.5 years of experience, specializing in React.js, Next.js, and JavaScript to build modern, responsive web applications. My skills include crafting user-friendly interfaces using Material-UI (MUI), Bootstrap, and Tailwind CSS, with a focus on achieving pixel-perfect designs. Proficient in state management with Redux, I ensure seamless data flow within applications. I am passionate about creating efficient, high-quality solutions and eager to contribute to innovative projects while collaborating with motivated teams to drive impactful results.
+            I am a Frontend Developer with 1.5 years of experience, specializing
+            in React.js, Next.js, React Native, and JavaScript. I have a passion
+            to build modern web applications and mobile apps. Skilled in
+            Material-UI, Tailwind CSS, and Bootstrap, I create pixel-perfect,
+            user-friendly interfaces. With expertise in Redux for state
+            management, I ensure seamless data flow and performance
+            optimization. Passionate about scalable, high-quality solutions, I
+            thrive in collaborative environments, contributing innovative ideas
+            to impactful projects.
           </p>
           <div className="hero-buttons">
-            <button className="btn-contact" onClick={() => window.location.href = "/contact"}>Contact</button>
-            <button className="btn-hire-me" onClick={() => window.location.href = "/contact"}>
+            <button
+              className="btn-contact"
+              onClick={() => (window.location.href = "/contact")}
+            >
+              Contact
+            </button>
+            <button
+              className="btn-hire-me"
+              onClick={() => (window.location.href = "/contact")}
+            >
               <span className="hire-me-span">Hire Me</span>
             </button>
           </div>
